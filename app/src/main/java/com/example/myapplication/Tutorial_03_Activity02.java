f43c36ed13d2ff63b3b0e2e9c9421b1524b0b1b6
Sumedha Lakmal Bandaranayaka <it20643218@my.sliit.lk>
2022-03-23 10:30:26 +0530

    Tutorial_03_Activity_02

---
+++ app/src/main/java/com/example/myapplication/Tutorial_03_Activity02.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tutorial_03_Activity02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial0302);
+    }
+}
