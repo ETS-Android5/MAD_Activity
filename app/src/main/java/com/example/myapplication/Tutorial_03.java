e44272ca278eb6ced4d7caeb72b113b5f831d8cb
Sumedha Lakmal Bandaranayaka <it20643218@my.sliit.lk>
2022-03-23 22:27:19 +0530

    Finished Tutorial 03

--- app/src/main/java/com/example/myapplication/Tutorial_03.java
+++ app/src/main/java/com/example/myapplication/Tutorial_03.java
@@ -2,13 +2,48 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Tutorial_03 extends AppCompatActivity {
+
+    EditText t_num1;
+    EditText t_num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial03);
+
+        t_num1 = findViewById(R.id.t_num1);
+        t_num2 = findViewById(R.id.t_num2);
+    }
+
+    //navigation part
+    public void openSecond(View view){
+        Intent intent = new Intent(this ,Tutorial_03_Activity02.class);
+
+        String num1 = t_num1.getText().toString();
+        String num2 = t_num2.getText().toString();
+
+        intent.putExtra("num1",num1);
+        intent.putExtra("num2",num2);
+
+        //layout
+        LayoutInflater li = getLayoutInflater();
+        View layout = li.inflate(R.layout.custom_toast,findViewById(R.id.te_calculater));
+
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM,0,0);
+        toast.setView(layout);
+        toast.show();
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/Tutorial_03_Activity02.java
+++ app/src/main/java/com/example/myapplication/Tutorial_03_Activity02.java
@@ -2,13 +2,60 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Tutorial_03_Activity02 extends AppCompatActivity {
+
+    EditText t_num1;
+    EditText t_num2;
+    TextView tv_answer;
+
+    String number1;
+    String number2;
+
+    int num1;
+    int num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial0302);
+
+        t_num1 = findViewById(R.id.t_num1);
+        t_num2 = findViewById(R.id.t_num2);
+        tv_answer = findViewById(R.id.tv_answer);
+
+        Intent intent = getIntent();
+
+        //receive value
+        number1 = intent.getStringExtra("num1");
+        number2 = intent.getStringExtra("num2");
+
+        t_num1.setText(number1);
+        t_num2.setText(number2);
+
+        num1 = Integer.parseInt(number1);
+        num2 = Integer.parseInt(number2);
+    }
+
+    //Calculation
+    public void add (View view){
+        tv_answer.setText(number1 + "+" + number2 + "+" + (num1+num2));
+    }
+
+    public void subtract (View view){
+        tv_answer.setText(number1 + "-" + number2 + "-" + (num1-num2));
+    }
+
+    public void multiply (View view){
+        tv_answer.setText(number1 + "*" + number2 + "*" + (num1*num2));
+    }
+
+    public void divide (View view){
+        tv_answer.setText(number1 + "/" + number2 + "/" + (num1/num2));
     }
 }
