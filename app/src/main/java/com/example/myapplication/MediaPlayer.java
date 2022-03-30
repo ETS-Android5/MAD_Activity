ff83d47139ed07bc265ab48e7d133d35a42a2485
Sumedha Lakmal Bandaranayaka <it20643218@my.sliit.lk>
2022-03-30 12:48:26 +0530

    Media Player

---
+++ app/src/main/java/com/example/myapplication/MediaPlayer.java
@@ -0,0 +1,39 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+public class MediaPlayer extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_player);
+    }
+
+    public void play (View view){
+        if(mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(getApplicationContext());
+            mediaPlayer.start();
+
+        }else{
+            mediaPlayer.start();
+        }
+    }
+
+    public void pause(View view){
+        if(mediaPlayer!= null){
+            mediaPlayer.pause();
+        }
+    }
+
+    public void stop(View view){
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+}
